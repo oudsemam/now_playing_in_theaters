2c9fad0f9331a5ebbd6b66986023dfac2e37016e
Maggie Oudsema <maggieoudsema@gmail.com>
2025-02-25 17:58:06 -0500

    adding a FAKE secrets file that has the formating correct

---
+++ secrets.py
@@ -0,0 +1,9 @@
+secrets = { 
+    'ssid' : 'YOUR_WIFI_NAME', 
+    'password' : 'YOUR_WIFI_PASSWORD', 
+    'aio_username' : 'YOUR_AIO_USERNAME', 
+    'aio_key' : 'YOUR_ADAFRUIT_AIO_KEY', 
+    'tmdb_key' : 'your_tmdb_key', 
+    'tmdb_access_token' : 
+    'your_tmdb_access_token', 
+    }
